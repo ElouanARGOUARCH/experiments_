54c8059d8509bf5db04dc5da8b417e78567aaa0d
EA264728 <elouan.argouarch@cea.fr>
2022-08-02 16:43:12 +0200

    mnist nsf

---
+++ MNIST/mnist_nsf.py
@@ -0,0 +1,32 @@
+import torch
+from models_nf import NeuralSplineFlow
+
+
+import torchvision.datasets as datasets
+import matplotlib.pyplot as plt
+mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
+mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
+images = mnist_trainset.data.flatten(start_dim=1)
+targets = mnist_trainset.targets
+
+digit = 'all'
+if digit != 'all':
+    extracted = images[targets == digit].float()
+else:
+    extracted = images.float()
+target_samples = extracted + torch.rand_like(extracted)/256
+
+num_samples = target_samples.shape[0]
+print('number of samples = ' + str(num_samples))
+p = target_samples.shape[-1]
+
+nsf = NeuralSplineFlow(target_samples, 20,256,3)
+
+epochs = 1000
+batch_size = 6000
+nsf.train(epochs, batch_size)
+
+filename = 'nsf_mnist2.sav'
+torch.save(nsf, filename)
+
+
