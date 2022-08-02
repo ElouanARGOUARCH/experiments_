6f9e9e3e63b7ba616a8c0a5523d3d1cbbadf04d6
EA264728 <elouan.argouarch@cea.fr>
2022-08-02 10:54:31 +0200

    mnist

--- MNIST/retrain.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-model = torch.load("dif_mnist.sav")
-print(model.loss_values[-1])
-epochs = 1000
-batch_size = 6000
-model.train(epochs, batch_size)
-filename = 'dif_mnist.sav'
-torch.save(model, filename)
