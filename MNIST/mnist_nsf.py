005d9a4153ab2fcd90cc6769f63a7a64dada2788
EA264728 <elouan.argouarch@cea.fr>
2022-08-09 14:15:14 +0200

    mnist best

--- MNIST/mnist_nsf.py
+++ MNIST/mnist_nsf.py
@@ -1,11 +1,9 @@
 import torch
-from models_nf import NeuralSplineFlow
+from models_nf import RealNVPDensityEstimatorLayer, MixedModelDensityEstimator
 
 
 import torchvision.datasets as datasets
-import matplotlib.pyplot as plt
 mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
-mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
 images = mnist_trainset.data.flatten(start_dim=1)
 targets = mnist_trainset.targets
 
@@ -14,19 +12,21 @@ if digit != 'all':
     extracted = images[targets == digit].float()
 else:
     extracted = images.float()
-target_samples = extracted + torch.rand_like(extracted)/256
+target_samples = (extracted + torch.rand_like(extracted))/256
 
 num_samples = target_samples.shape[0]
 print('number of samples = ' + str(num_samples))
 p = target_samples.shape[-1]
 
-nsf = NeuralSplineFlow(target_samples, 20,128,3)
+structure = [[RealNVPDensityEstimatorLayer,[256,256,256]] for i in range(10)]
+rnvp = MixedModelDensityEstimator(target_samples, structure)
+print(rnvp.compute_number_params())
 
 epochs = 1000
 batch_size = 6000
-nsf.train(epochs, batch_size)
+rnvp.train(epochs, batch_size)
 
 filename = 'nsf_mnist.sav'
-torch.save(nsf, filename)
+torch.save(rnvp, filename)
 
 
